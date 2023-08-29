9aa064d018ac034aee82fa1c8e31e20f37c51b58
farrelmaahira <farrelmaahira104@gmail.com>
2023-08-29 17:00:20 +0700

    new commit

--- routes/chat.js
+++ routes/chat.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const {User} = require('./../models/index')
 
 router.get('/chat', (req,res)=>{
   res.render('./../views/chat.ejs')
+})
+
+router.get('/chat/:id', async (req,res)=>{
+  const id = req.params.id
+  const data = await User.findOne({
+    where : {
+      id : id 
+    } 
+  })
+  
+  res.send(data)
 })
 
 module.exports = router
